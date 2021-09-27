05b02a95ce0046003cde6df7ad22ed99ec73d186
Ajay Prakash <ajaypup25@gmail.com>
2021-09-27 21:02:54 +0530

    Update script.js
    fixing bug of after char count

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
 let textBox = document.querySelector('#textBox');
 textBox.addEventListener('input',function(){
     let text = this.value;
+    text= text.trim();
     let char = text.length;
-    
-    document.querySelector('#char').innerHTML = char;
-     text= text.trim();
     let words = text.split(" ");
+    char = char-(words.length-1)
+    document.querySelector('#char').innerHTML = char;
     let cleanList= words.filter(function(el){
         return el !== "";
     })
